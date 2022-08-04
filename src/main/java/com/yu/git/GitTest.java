104a8833d31b3650b413749ef2c9b9d55fc1c042
HefangYu <110547630+HefangYu@users.noreply.github.com>
2022-08-04 17:11:22 +0800

    Update GitTest.java

--- src/main/java/com/yu/git/GitTest.java
+++ src/main/java/com/yu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello github master!");
         System.out.println("hello github hot fix!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
